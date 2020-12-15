6e3a01da080063ba01857fd83dc5627c672ba313
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-12-15 23:55:02 +0600

    line chart added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CryptoView from "@/views/CryptoView";
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/crypto',
+    name: 'Crypto',
+    component: CryptoView
   },
   {
     path: '/about',
--- src/views/CryptoView.vue
+++ src/views/CryptoView.vue
@@ -1,15 +1,39 @@
 <template>
     <div>
-
+        <LineChart/>
     </div>
 </template>
 
 <script>
+
+    import LineChart from "@/components/LineChart";
     export default {
-        name: "CryptoView"
+        name: "CryptoView",
+
+        components: {
+            LineChart
+
+        },
+        data () {
+            return {
+
+            }
+        },
+        mounted () {
+
+
+        },
+        methods: {
+
+        }
     }
 </script>
 
-<style scoped>
+<style >
+
+    .line-chart{
+        height: 150px;
+        /*margin:  150px auto;*/
+    }
 
 </style>
