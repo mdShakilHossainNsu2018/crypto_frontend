c2a954666323fa9ee540772e2e509b3c038ae988
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2021-02-18 17:44:07 +0600

    https://backend.ovexbroker.co.za/api/ added http

--- src/store/user/state.js
+++ src/store/user/state.js
@@ -3,7 +3,7 @@ const STATE = {
     data: null,
     // baseUrl: "http://localhost:8000/api/",
     // baseUrl: 'https://enigmatic-atoll-64574.herokuapp.com/api/',
-    baseUrl: 'https://backend.ovexbroker.co.za/api/',
+    baseUrl: 'http://backend.ovexbroker.co.za/api/',
     token: null,
     user: null,
     isLoading: false,
