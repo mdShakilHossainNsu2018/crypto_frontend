7ed3bd786a2c4b2559fc672332cc6c4168dda7f1
mdShakilHossainNsu2018 <shakilnsu2018@gmail.com>
2021-02-21 19:34:17 +0600

    base url changed

--- src/store/user/state.js
+++ src/store/user/state.js
@@ -3,7 +3,7 @@ const STATE = {
     data: null,
     // baseUrl: "http://localhost:8000/api/",
     // baseUrl: 'https://enigmatic-atoll-64574.herokuapp.com/api/',
-    baseUrl: 'http://backend.ovexbroker.co.za/api/',
+    baseUrl: 'https://powerful-basin-71452.herokuapp.com/api/',
     token: null,
     user: null,
     isLoading: false,
